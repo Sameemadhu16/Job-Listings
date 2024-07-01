aaf4dc80fa19d743eebe77bc658807900d7d840b
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-07-01 23:18:48 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect("mongodb+srv://ilakshitha7921:Ishan2001@cluster0.oyqzkn8.mongodb.net/jobwave?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+  console.log('MongoDB is conected');
+})
+.catch((err) =>
+{
+  console.log(err);
+})
 
 const app = express();
 
 app.listen(4500,() => {
     console.log('Server is running port 4500');
 });
+
