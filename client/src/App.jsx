4ea51afba4960a9a442c0a12441925e0c1ff9693
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-07-03 18:46:25 +0530

    create home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Signup from './Signup'
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
 
 export default function App() {
   return <BrowserRouter>
   <Routes>
-    <Route path='/sign-up' element={<Signup/>}/>
+  <Route path='/' element={<Home/>}/>
+    <Route path='/sign-up' element={<SignUp/>}/>
   </Routes>
 </BrowserRouter>
     
