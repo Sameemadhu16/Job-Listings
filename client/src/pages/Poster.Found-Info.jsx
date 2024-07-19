66479ca0e37df685f1429640a1242bdd4b8a4798
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-07-19 14:59:27 +0530

    complete code

--- client/src/pages/Poster.Found-Info.jsx
+++ client/src/pages/Poster.Found-Info.jsx
@@ -4,23 +4,24 @@ import { FaArrowRight } from "react-icons/fa";
 
 export default function PosterCompanyInfo() {
   return (
-    <div className='p-3 max-w-3xl mx-auto min-h-screen bg-gray-100'>
-    <form className="flex flex-col flex-wrap gap-4 m-10" >
-        <h1 className='text-3xl font-bold'>
-            Found Info :
-        </h1>
-       <div className='flex flex-row gap-2'>
-       <div className=''>
-        <p className='text-sm font-semibold'>Organization Types :</p>
-            <Select className='mt-1'>
-                <option value="uncategorized">Select a category</option>
-                <option value="javascript">JavaScript</option>
-                <option value="react.js">React.js</option>
-                <option value="next.js">Next.js</option>
-                <option value="node.js">node.js</option>
-                <option value="dart">Dart</option>
+    <div className='bg-slate-100'>
+        <div className='p-3 max-w-3xl mx-auto min-h-screen bg-gray-100'>
+        <form className="flex flex-col flex-wrap gap-4 m-10" >
+            <h1 className='text-3xl font-bold'>
+                Found Info :
+            </h1>
+        <div className='flex flex-row gap-2'>
+        <div className=''>
+            <p className='text-sm font-semibold'>Organization Types :</p>
+                <Select className='mt-1'>
+                    <option value="uncategorized">Select a category</option>
+                    <option value="javascript">JavaScript</option>
+                    <option value="react.js">React.js</option>
+                    <option value="next.js">Next.js</option>
+                    <option value="node.js">node.js</option>
+                    <option value="dart">Dart</option>
 
-            </Select>
+                </Select>
        </div>
 
        <div className=''>
@@ -65,18 +66,19 @@ export default function PosterCompanyInfo() {
        </div>
         
     </form>
-    <div className='flex gap-3 m-10'>
-        <Button type='button' cols={50} className='mt-3 bg-slate-400 px-5' >
-            <div className='flex flex-row gap-1 items-center'>
-            Previous
-            </div>
-        </Button>
-        <Button type='button' cols={50} className='bg-blue-500 hover:bg-opacity-95 mt-3' >
-            <div className='flex flex-row gap-1 items-center'>
-            Save & Next
-            <FaArrowRight />
-            </div>
-        </Button>
+        <div className='flex gap-3 m-10'>
+            <Button type='button' cols={50} className='mt-3 bg-slate-400 px-5' >
+                <div className='flex flex-row gap-1 items-center'>
+                Previous
+                </div>
+            </Button>
+            <Button type='button' cols={50} className='bg-blue-500 hover:bg-opacity-95 mt-3' >
+                <div className='flex flex-row gap-1 items-center'>
+                Save & Next
+                <FaArrowRight />
+                </div>
+            </Button>
+        </div>
     </div>
     </div>
   )
