13895dd8263bd303174511d24bab4215dd6d7042
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-07-19 22:04:22 +0530

    add poster pages

---
+++ client/src/components/PostCards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PostCards() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,27 @@
+import { Button, TextInput } from 'flowbite-react'
 import React from 'react'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { IoMdNotifications } from 'react-icons/io'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <form className='mr-3'>
+            <TextInput type='text'
+            placeholder='Search Job...'
+            rightIcon={AiOutlineSearch}
+            className='hidden lg:inline mr-4'/>
+        </form>
+        <Button className='w-12 h-10 lg:hidden focus:outline-none' color='gray' pill>
+            <AiOutlineSearch/>
+        </Button>
+  <Button className='mr-1'>Filter</Button>
+  <Button className='mr-10'>Find Job</Button>
+  <div>
+    
+  </div>
+  
+        
+</div>
   )
 }
