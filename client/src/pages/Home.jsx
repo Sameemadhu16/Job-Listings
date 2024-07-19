129a8d40ef665f4c795be653e2f49bf3d53487e9
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-07-20 00:39:57 +0530

    create seeker home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,45 +1,49 @@
-import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
-import { AiOutlineSearch } from 'react-icons/ai'
-import { IoMdNotifications } from 'react-icons/io';
+import { Button, TextInput } from 'flowbite-react';
+import React from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
 import { TbFilterBolt } from "react-icons/tb";
 import PostCards from '../components/PostCards';
 
 export default function Home() {
   return (
-      <div >
-    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <form className='mr-3'>
-            <TextInput type='text'
-            placeholder='Search Jobs...'
+    <div className="min-h-screen bg-gray-100">
+      <div className="flex justify-end p-4 bg-white shadow">
+        <form className="mr-3 hidden lg:inline-block">
+          <TextInput
+            type="text"
+            placeholder="Search Jobs..."
             rightIcon={AiOutlineSearch}
-            className='hidden lg:inline mr-4'/>
+            className="mr-4"
+          />
         </form>
-        <Button className='w-50 h-20 lg:hidden focus:outline-none' color='gray' pill>
-            <AiOutlineSearch/>
+        <Button className="w-12 h-12 lg:hidden focus:outline-none" color="gray" pill>
+          <AiOutlineSearch />
         </Button>
-  <Button className='mr-10 ml-10' >
-  <TbFilterBolt />
-    Filter
-  
-  </Button>
-  <Button className='mr-8'>Find Job</Button>
-  
-        
-</div>
-  <div className='grid grid-cols-1 sm:grid-cols-3 gap-6 p-6'>
-  <PostCards/>
-  <PostCards/>
-  <PostCards/>
-  <PostCards/>
-  <PostCards/>
-  <PostCards/>
-  </div>
-  <div className='justify-center'>
-   <h1>Ishan Lakshitha</h1>
-  </div>
-  
-</div>
-
-  )
+        <Button className="flex flex-col items-center mx-4">
+          <TbFilterBolt />
+          <span>Filter</span>
+        </Button>
+        <Button className="mx-4">Find Job</Button>
+      </div>
+      <div className="p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <PostCards />
+          <PostCards />
+          <PostCards />
+          <PostCards />
+          <PostCards />
+          <PostCards />
+        </div>
+      </div>
+      <div className="flex justify-center py-4">
+        <Button.Group>
+          <Button color="light">01</Button>
+          <Button color="light">02</Button>
+          <Button color="light">03</Button>
+          <Button color="light">04</Button>
+          <Button color="light">05</Button>
+        </Button.Group>
+      </div>
+    </div>
+  );
 }
