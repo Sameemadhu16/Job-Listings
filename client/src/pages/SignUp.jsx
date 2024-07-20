b89a5a94ddde4d2f5a1d3e4fe8e6f4d2f4f53001
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-07-21 01:34:36 +0530

    make changes sign up pages

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import SignUpStep2 from './SignUpStep2';
-import  { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Button, Label, TextInput } from 'flowbite-react';
 
 const SignUp = () => {
@@ -39,84 +39,91 @@ const SignUp = () => {
     };
 
     return (
-        <div className="flex flex-col justify-center p-8">
+        <div className="flex flex-col justify-center p-8 bg-gray-100">
             {step === 1 && (
-                <div className="w-full md:w-1/2 lg:w-1/2 flex flex-col justify-center p-6">
-                    <h2 className="text-3xl font-bold mb-4">Create account.</h2>
-                       
+                <div className="w-full flex flex-col md:flex-row md:items-center justify-center px-8">
+                    <div className="md:w-1/2 p-6">
+                        <h2 className="text-3xl font-bold mb-4">Create account.</h2>
                         <div className="flex gap-2 text-sm mt-4 mb-3">
-                        <span className=''>Have an account?</span>
-                                <NavLink to="/sign-in" className="text-blue-500">
-                                    Sign In
-                                </NavLink>
-                         </div>
-                    
-                    <form onSubmit={handleNextStep} className='flex flex-col' >
-                        <div className="flex mb-4 ">
-                            <Button
-                                className={`flex-1 p-3 border rounded-l-lg mr-2 ${formData.role === 'jobPoster' ? 'px-2 py-1 bg-blue-500 rounded-lg ' : 'border-gray-300 bg-gray-100 rounded-lg'}`}
-                                onClick={() => setFormData({ ...formData, role: 'jobPoster' })}
-                            >
-                                Job Poster
-                            </Button>
-                            <Button
-                                className={`flex-1 p-3 border rounded-r-lg ml-2 ${formData.role === 'jobSeeker' ? 'px-2 py-1 bg-blue-500 rounded-lg' : 'border-gray-300 bg-gray-100 rounded-lg'}`}
-                                onClick={() => setFormData({ ...formData, role: 'jobSeeker' })}
-                            >
-                                Job Seeker
-                            </Button>
+                            <span className=''>Have an account?</span>
+                            <NavLink to="/sign-in" className="text-blue-500">
+                                Sign In
+                            </NavLink>
                         </div>
-                        <div className='gap-2'>
-                            <div className='mb-4'>
-                                <TextInput type="text" placeholder="Full name" id="fullname" onChange={handleChange} />
-                            </div>
-                            <div className='mb-4'>
-                                <TextInput type="text" placeholder="Email" id="email" onChange={handleChange} />
-                            </div>
-                            <div className='mb-4'>
-                                <TextInput type="text" placeholder="Password" id="password" onChange={handleChange} />
-                            </div>
-                            <div className='mb-4'>
-                                <TextInput type="text" placeholder="Confirm Password" id="confirmpassword" onChange={handleChange} />
-                            </div>
-                            <div className='mb-4'>
-                                <TextInput type="text" placeholder="Mobile Number" id="mobilenumber" onChange={handleChange} />
-                            </div>
-                            <div className='mb-4'> 
-                                <select
-                                    name="gender"
-                                    value={formData.gender}
-                                    onChange={handleChange}
-                                    required
-                                    className="w-full p-3 border rounded"
-                                >
-                                    <option value="" disabled>Select Gender</option>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                    <option value="other">Other</option>
-                                </select> 
-                            </div>
-                                <div className="mb-4 rounded-lg">
-                                    <input
-                                        type="date"
-                                        name="birthday"
-                                        value={formData.birthday}
-                                        onChange={handleChange}
-                                        required
-                                        className="w-full p-3 border rounded"
-                                    />
+                        <div>
+                            <form onSubmit={handleNextStep} className='flex flex-col'>
+                                <div className="flex mb-4 ">
+                                    <Button
+                                        className={`flex-1 p-3 border rounded-l-lg mr-2 ${formData.role === 'jobPoster' ? 'px-2 py-1 bg-blue-500 rounded-lg ' : 'bg-violet-300 border-blue-500 text-black'}`}
+                                        onClick={() => setFormData({ ...formData, role: 'jobPoster' })}
+                                    >
+                                        Job Poster
+                                    </Button>
+                                    <Button
+                                        className={`flex-1 p-3 border rounded-r-lg ml-2 ${formData.role === 'jobSeeker' ? 'px-2 py-1 bg-blue-500 rounded-lg' : 'bg-violet-300  border-blue-500 text-black'}`}
+                                        onClick={() => setFormData({ ...formData, role: 'jobSeeker' })}
+                                    >
+                                        Job Seeker
+                                    </Button>
                                 </div>
-                            </div>
-                        
-                        <div className="mb-4">
-                            <Button type="submit" className="flex-justify-center w-20 bg-blue-500 text-white rounded">Next</Button>
+                                <div className='gap-2'>
+                                    <div className='mb-4'>
+                                        <Label value="Your Full Name" />
+                                        <TextInput type="text" placeholder="Full Name" id="fullname"  onChange={handleChange} />
+                                    </div>
+                                    <div className='mb-4'>
+                                        <Label value="Your Email" />
+                                        <TextInput type="text" placeholder="Email" id="email" onChange={handleChange} />
+                                    </div>
+                                    <div className='mb-4'>
+                                        <Label value="Your Password" />
+                                        <TextInput type="text" placeholder="**********" id="password"  onChange={handleChange} />
+                                    </div>
+                                    <div className='mb-4'>
+                                        <Label value="Confirm Password" />
+                                        <TextInput type="text" placeholder="**********" id="confirmpassword"  onChange={handleChange} />
+                                    </div>
+                                    <div className='mb-4'>
+                                        <Label value="Your Mobile Number" />
+                                        <TextInput type="text" placeholder="Mobile Number" id="mobilenumber" onChange={handleChange} />
+                                    </div>
+                                    <div className='mb-4'>
+                                        <select
+                                            name="gender"
+                                            value={formData.gender}
+                                            onChange={handleChange}
+                                            required
+                                            className="w-full p-3 border rounded"
+                                        >
+                                            <option value="" disabled>Select Gender</option>
+                                            <option value="male">Male</option>
+                                            <option value="female">Female</option>
+                                            <option value="other">Other</option>
+                                        </select>
+                                    </div>
+                                    <div className="mb-4 rounded-lg">
+                                        <input
+                                            type="date"
+                                            name="birthday"
+                                            value={formData.birthday}
+                                            onChange={handleChange}
+                                            required
+                                            className="w-full p-3 border rounded"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="mb-4">
+                                    <Button type="submit" className="flex-justify-center w-20 bg-blue-500 text-white rounded">Next</Button>
+                                </div>
+                            </form>
                         </div>
-                        
-                    </form>
-                    
-                    <p className="text-center mb-4 mt-2">OR</p>
-                    <div className="flex justify-center">
-                        <button className=" p-3 border rounded mx-2">Sign up with Google</button>
+                        <p className="text-center mb-4 mt-2">OR</p>
+                        <div className="flex justify-center">
+                            <button className="p-3 border rounded mx-2">Sign up with Google</button>
+                        </div>
+                    </div>
+                    <div className="md:w-1/2 p-6 flex justify-center">
+                        <img src="https://img.freepik.com/free-photo/copy-space-blank-commercial-advertisement_53876-121262.jpg?t=st=1721500082~exp=1721503682~hmac=84ee8a13f47eef8a6ff863cfd1ec73b8c03072111099630aafb8d00e04250362&w=740" alt="Sign Up Illustration" className="w-full shadow-md"/>
                     </div>
                 </div>
             )}
--- client/src/pages/SignUpStep2.jsx
+++ client/src/pages/SignUpStep2.jsx
@@ -11,28 +11,30 @@ const SignUpStep2 = ({ formData, setFormData, handleSubmit }) => {
     };
 
     return (
-        <div className="flex flex-col justify-center p-8 ">
-            <h2 className="text-3xl font-bold mb-4">Complete your profile</h2>
-            <div>
+        <div className="flex flex-col md:flex-row justify-center p-8">
+            <div className="md:w-1/2 flex flex-col justify-center p-6 py-6">
+                <h2 className="text-3xl font-bold mb-4">Complete your profile</h2>
                 <form onSubmit={handleSubmit}>
                     {formData.role === 'jobPoster' && (
-                        <div className='w-full md:w-1/2 lg:w-1/2 flex flex-col justify-center p-6'>
+                        <div>
                             <div className='mb-4'>
-                                    <TextInput type="text" placeholder="Company Name" id="companyname" onChange={handleChange} />
+                                <Label value="Your Company Name" />
+                                <TextInput type="text" placeholder="Company Name" id="companyname" name="companyName" value={formData.companyName} onChange={handleChange} />
                             </div>
                             <div className='mb-4'>
-                                    <TextInput type="text" placeholder="Company Website" id="companywebsite" onChange={handleChange} />
+                                <Label value="Your Company Website" />
+                                <TextInput type="text" placeholder="Company Website" id="companywebsite" name="companyWebsite" value={formData.companyWebsite} onChange={handleChange} />
                             </div>
                             <div className='mb-4'>
-                                    <TextInput type="text" placeholder="Biograohy" id="biography"value={formData.biography} onChange={handleChange} required className="w-full p-3 border rounded"/>
+                                <Label value="Biography" />
+                                <TextInput type="text" placeholder="Biography" id="biography" name="biography" value={formData.biography} onChange={handleChange} required className="w-full p-3 border rounded"/>
                             </div>
                             <div className='mb-4'>
-                                    <TextInput type="text" placeholder="Cover Letter" id="coverletter" value={formData.coverLetter} onChange={handleChange} required className="w-full p-3 border rounded"/>
+                                <Label value="Cover Letter" />
+                                <TextInput type="text" placeholder="Cover Letter" id="coverletter" name="coverLetter" value={formData.coverLetter} onChange={handleChange} required className="w-full p-3 border rounded"/>
                             </div>
-                    
                         </div>
                     )}
-
                     {formData.role === 'jobSeeker' && (
                         <div>
                             <div className="mb-4">
@@ -56,27 +58,26 @@ const SignUpStep2 = ({ formData, setFormData, handleSubmit }) => {
                                 />
                             </div>
                         </div>
-                    )}  
+                    )}
                     <div className="mb-4 flex items-center">
-                                <TextInput
-                                    type="checkbox"
-                                    name="terms"
-                                    required
-                                    className="mr-2"
-                                />
-                                <Label>
-                                    I've read and agree with your terms and conditions
-                                </Label>
-                    </div> 
-
-                    <div className="mb-4 w-full md:w-1/2 lg:w-1/2 flex flex-col justify-center p-6">
-                        <Button type="submit" className="w-full p-3 bg-blue-500 text-white rounded-lg">Create Account</Button>
-                    </div>
+                        <TextInput
+                            type="checkbox"
+                            name="terms"
+                            required
+                            className="mr-2"
+                        />
+                        <Label>
+                            I've read and agree with your terms and conditions
+                        </Label>
+                    </div>    
                 </form>
-
+                <div className="mb-4">
+                        <Button type="submit" className="w-full p-3 bg-blue-500 text-white rounded-lg mt-20">Create Account</Button>
+             </div>
             </div>
-
-            
+            <div className="md:w-1/2 p-6 flex justify-center">
+                <img src="https://img.freepik.com/free-photo/copy-space-blank-commercial-advertisement_53876-121262.jpg?t=st=1721500082~exp=1721503682~hmac=84ee8a13f47eef8a6ff863cfd1ec73b8c03072111099630aafb8d00e04250362&w=740" alt="Sign Up Illustration" className="w-full h-fit shadow-md"/>
+            </div>
             
         </div>
     );
