ec33da7a2d694b841aa815382e5e564a11501d8a
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-07-21 10:01:51 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,9 +4,9 @@ import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
-    const { username, fullname, email, password, role, gender, mobileNumber, birthday, maritalStatus, experience, education, biography, coverLetter, cv, skills, companyName } = req.body;
+    const { username, email, password, role, gender, mobileNumber, birthday, maritalStatus, experience, education, biography, coverLetter, resume, skills, companyName } = req.body;
 
-    if (!username || !fullname || !email || !password || !role || username === '' || fullname === '' || email === '' || password === '' || role === '') {
+    if (!username || fullname || !email || !password || !role || username === '' || fullname === '' || email === '' || password === '' || role === '') {
         return next(errorHandler(400, 'All feilds are requiired'));
     }
 
@@ -38,31 +38,4 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 
-};
-
-export const signin = async (req, res, next) => {
-    const { email, password } = req.body;
-
-    if (!email || !password || email === '' || password === '') {
-        next(errorHandler(400, 'All fields are required'));
-    }
-    try {
-        const validUser = await User.findOne({ email });
-        if (!validUser) {
-            return next(errorHandler(400, 'User not found'));
-        }
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) {
-            return next(errorHandler(400, 'Invalid Password'));
-        }
-        const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin, isModerator: validUser.isModerator }, process.env.JWT_SECRET);
-
-        const { password: pass, ...rest } = validUser._doc;
-
-        res.status(200).cookie('access_token', token, {
-            httpOnly: true
-        }).json(rest);
-    } catch (error) {
-        next(error);
-    }
 };
