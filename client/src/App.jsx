e083a4606b755246d14a6a0c41702f26aa47a502
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-07-27 12:12:21 +0530

    complete full time job post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,12 +13,6 @@ import Finish from './pages/Finish';
 import PosterDashboard from './pages/Poster.Dashboard';
 import SeekerDashboard from './pages/Seeker.Dashboard';
 import JobPost from './components/JobPost';
-import PosterDashOverview from './components/Poster.DashOverview';
-import PosterSetting from './components/poster.Setting';
-import PosterCompanyInfo from './components/Poster.CompanyInfo';
-import PosterPostjob from './components/Poster.Postjob';
-import PosterFoundInfo from './components/Poster.FoundInfo';
-import PosterSocialMediaInfo from './components/Poster.SocialMediaInfo';
 
 
 export default function App() {
@@ -30,23 +24,12 @@ export default function App() {
       <Route path='/sign-up' element={<SignUp />} />
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/company-info' element={<PosterCompanyInfo />} />
+      <Route path='/found-info' element={<PosterFountInfo />} />
+      <Route path='/social-info' element={<PosterSocailMediaInfo />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/finish' element={<Finish />} />
-      <Route path='/poster-dashboard' element={<PosterDashboard />} >
-        <Route index element={<PosterDashOverview />} />
-        <Route path='employeeprofile' element={<PosterDashOverview />} />
-        <Route path='post-jobs' element={<PosterPostjob />} >
-          <Route index element={<PosterCompanyInfo />} />
-          <Route path='founding' element={<PosterFoundInfo />} />
-          <Route path='social-media' element={<PosterSocialMediaInfo />} />
-          <Route path='account-setting' element={<PosterCompanyInfo />} />
-        </Route>
-        <Route path='settings' element={<PosterSetting />}>
-          <Route index element={<PosterCompanyInfo />} />
-
-        </Route>
-      </Route>
-      <Route path='/company' element={<PosterCompanyInfo />} />
+      <Route path='/poster-dashboard' element={<PosterDashboard />} />
 
       <Route path='/seeker-dashboard' element={<SeekerDashboard />} />
       <Route path='/job-post' element={<JobPost />} />
