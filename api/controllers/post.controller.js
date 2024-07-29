2102e1216470d57db3a4bb5dfe2c71ba0536677c
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-07-29 19:51:50 +0530

    create and test crud operations in posts

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,62 @@
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const createPost = async(req,res,next) => {
+    if(!req.body.essential){
+        return next(errorHandler(400,'You are not allowed to create a post'))
+    }
+
+    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g,'-');
+    const newPost = new Post({
+        ...req.body,
+        slug,
+        //userId:req.user.id,
+    });
+
+    try{
+        const savedPost = await newPost.save();
+        res.status(201).json(savedPost);
+    }catch(error){
+        next(error);
+    }
+}
+
+export const updatePost = async(req,res,next) => {
+    try{
+        const updatePost = await Post.findByIdAndUpdate(
+            req.params.postId,
+            {
+                $set:{
+                    title:req.body.title,
+                    essential:req.body.title,
+                    selectType:req.body.selectType,
+                    description:req.body.description
+                }
+            },{new:true}
+        )
+        res.status(200).json(updatePost)
+    }catch(error){
+        next(error);
+    }
+}
+
+export const deletePost = async(req,res,next) => {
+    try{
+        const deletePost = await Post.findByIdAndDelete(req.params.postId);
+        res.status(200).json('This post has been deleted');
+    }catch(error){
+        next(error);
+    }
+}
+
+export const getPosts = async  (req,res,next) => {
+    try{
+        const totalPosts = await Post.countDocuments();
+        
+        res.status(200).json({
+            totalPosts,
+        })
+    }catch (error) {
+        next(error);
+    }
+};
---
+++ api/models/post.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { TiThLarge } from "react-icons/ti";
+
+const postSchema = new mongoose.Schema(
+    {
+        /*userId:{
+            type:String,
+            required:true
+        },*/
+        title:{
+            required:true,
+            type:String,
+            unique:true
+        },
+        essential:{
+            type:String
+        },
+        selectType:{
+            type:String
+        },
+        description:{
+            type:String,
+        },
+        companyName:{
+            type:String,
+            required:true
+        }
+
+    },{timestamps:true}
+);
+
+const Post = mongoose.model('Post',postSchema);
+export default Post
