021220106ca3e08bc736c742cf23d42360324906
irumi <irumiabeywickrama@gmail.com>
2024-07-31 08:09:11 +0530

    Add applied jobs and cart for seeker model

--- api/index.js
+++ api/index.js
@@ -4,46 +4,44 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import seekerRoutes from './routes/seeker.route.js'
+
 import cookieParser from 'cookie-parser';
 import path from 'path';
-import postRoutes from './routes/post.route.js'
 
+import postRoutes from './routes/post.route.js'
 
 dotenv.config();
 
 mongoose.connect("mongodb+srv://ilakshitha7921:ilakshitha7921@cluster0.gfhczos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+  console.log('MongoDB is conected');
+})
+.catch((err) =>
+{
+  console.log(err);
+});
 
-
-
-  .then(() => {
-    console.log('MongoDB is conected');
-  })
-  .catch((err) => {
-    console.log(err);
-
-  });
-
-
-
-
-
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(4500, () => {
-  console.log('Server is running port 4500');
+app.listen(4500,() => {
+    console.log('Server is running port 4500');
 });
 
 app.use('/api/user', userRoutes);
-
 app.use('/api/auth', authRoutes);
 app.use('/api/seeker', seekerRoutes)
-app.use('/api/auth', authRoutes);
 
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -53,9 +51,7 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
-
 });
 
 app.use('/api/post',postRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/post', postRoutes);
+
