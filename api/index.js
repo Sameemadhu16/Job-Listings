c3f7e2c8a023ce60bbdac62a66d43ed2b1f75901
irumi <irumiabeywickrama@gmail.com>
2024-07-31 08:09:11 +0530

    Add applied jobs and cart for seeker model

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import seekerRoutes from './routes/seeker.route.js'
 
 import cookieParser from 'cookie-parser';
 import path from 'path';
@@ -33,6 +34,7 @@ app.listen(4500,() => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/seeker', seekerRoutes)
 
 
 
--- api/routes/seeker.route.js
+++ api/routes/seeker.route.js
@@ -1,9 +1,7 @@
 import express from 'express'
-import { addappliedjobs, addcart, getJobs } from '../controllers/seeker.controller.js'
+import { getJobs } from '../controllers/seeker.controller.js'
 
 const router = express.Router();
 router.get('/getjobs', getJobs);
-router.get('/getjobs/:postId',getJobs)
-router.post('/addcart/:userId', addcart);
-router.post('/addapplied/:userId', addappliedjobs);
+
 export default router;
