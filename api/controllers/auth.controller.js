16d41bb10ab9a5fc5e65c38da6d1a1bfd81139c7
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-08-04 14:29:03 +0530

    complete sign in and sign up

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req , res, next) => {
-    const { username,fullname, email, password, role,gender, mobileNumber, birthday, maritalStatus, experience,education,biography,coverLetter,cv,skills,companyName,cart,appliedjobs} = req.body;
+    const { username,fullname, email, password, role,gender, mobileNumber, birthday, maritalStatus, experience,education,biography,coverLetter,cv,skills,companyName} = req.body;
 
     if (!username ||!fullname || !email || !password || !role || username === '' || fullname === ''|| email === '' || password === ''|| role === ''){
         return next(errorHandler(400, 'All feilds are requiired'));
@@ -29,9 +29,6 @@ export const signup = async (req , res, next) => {
         coverLetter: role === 'jobPoster' ? coverLetter : undefined,
         cv: role === 'jobSeeker' ? cv : undefined,
         skills: role === 'jobSeeker' ? skills : undefined,
-        cart: role === 'jobSeeker' ? cart : undefined,
-        appliedjobs: role === 'jobSeeker' ? appliedjobs : undefined,
-
     });
 
     try{
