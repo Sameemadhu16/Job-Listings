ebf23cc932ad10d56e609847cc0734b801e0f964
irumi <irumiabeywickrama@gmail.com>
2024-08-04 13:49:07 +0530

    modify index.js for backend working

--- api/index.js
+++ api/index.js
@@ -4,22 +4,27 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import seekerRoutes from './routes/seeker.route.js'
-
 import cookieParser from 'cookie-parser';
 import path from 'path';
-
 import postRoutes from './routes/post.route.js'
+
 
 dotenv.config();
 
 mongoose.connect("mongodb+srv://ilakshitha7921:ilakshitha7921@cluster0.gfhczos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-  console.log('MongoDB is conected');
-})
-.catch((err) =>
-{
-  console.log(err);
-});
+
+
+
+  .then(() => {
+    console.log('MongoDB is conected');
+  })
+  .catch((err) => {
+    console.log(err);
+
+  });
+
+
+
 
 
 
@@ -28,14 +33,15 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(4500,() => {
-    console.log('Server is running port 4500');
+app.listen(4500, () => {
+  console.log('Server is running port 4500');
 });
 
 app.use('/api/user', userRoutes);
+
 app.use('/api/auth', authRoutes);
 app.use('/api/seeker', seekerRoutes)
-
+app.use('/api/auth', authRoutes);
 
 
 
@@ -47,7 +53,11 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+
 });
 
 app.use('/api/post',postRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+
 
