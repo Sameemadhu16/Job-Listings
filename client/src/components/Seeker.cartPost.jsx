fbeb691ee19cdd07f6b20496071d678b7be1cd89
irumi <irumiabeywickrama@gmail.com>
2024-08-05 11:07:51 +0530

    getting applied jobs and latest jobs

--- client/src/components/Seeker.cartPost.jsx
+++ client/src/components/Seeker.cartPost.jsx
@@ -3,15 +3,10 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import CompanyDetailsModal from './Seeker.CompanyDetailsModal';
 import { useSelector } from 'react-redux';
-import DashComments from './Seeker.commentsection';
 
 export default function SeekerCartPost({ ShowAddcart, showApply, showDelete, post }) {
   const { currentUser } = useSelector((state) => state.user);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isCommentModalOpen, setIsCommentModalOpen] = useState(false);
-  const [cartPostIdToDelete, setCartPostIdToDelete] = useState('');
-  const [showAlert, setShowAlert] = useState(false);
-
 
   const handleModalOpen = () => {
     setIsModalOpen(true);
@@ -19,14 +14,6 @@ export default function SeekerCartPost({ ShowAddcart, showApply, showDelete, pos
 
   const handleModalClose = () => {
     setIsModalOpen(false);
-  };
-
-  const handleCommentModalOpen = () => {
-    setIsCommentModalOpen(true);
-  };
-
-  const handleCommentModalClose = () => {
-    setIsCommentModalOpen(false);
   };
 
   const handleCart = async () => {
@@ -49,49 +36,12 @@ export default function SeekerCartPost({ ShowAddcart, showApply, showDelete, pos
       }
 
       console.log('Post added to cart successfully');
-      setShowAlert(true);
 
-          // Hide the alert after 3 seconds
-      setTimeout(() => {
-        setShowAlert(false);
-      }, 3000);
-      
     } catch (error) {
       console.error('Error adding post to cart:', error.message);
       // Handle error: display error message or perform other actions
     }
   };
-  const handleAppliedjobs = async () => {
-    try {
-      if (!currentUser || !currentUser._id) {
-        throw new Error('User ID not available');
-      }
-
-      const res = await fetch(`/api/seeker/addapplied/${currentUser._id}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ postId: post._id }), // Assuming post._id is available from props
-      });
-
-      if (!res.ok) {
-        const data = await res.json();
-        throw new Error(data.message || 'Failed to add post to applied list');
-      }
-
-      console.log('Post added to apllied list successfully');
-    } catch (error) {
-      console.error('Error adding post to applied list:', error.message);
-      // Handle error: display error message or perform other actions
-    }
-    
-  };
-  const handleApplyButtonClick = () => {
-    handleModalOpen();
-    handleAppliedjobs();
-  };
-  
 
   const date = new Date(post.createdAt);
 
@@ -112,12 +62,6 @@ export default function SeekerCartPost({ ShowAddcart, showApply, showDelete, pos
 
   return (
     <div className='group relative w-full sm:w-[380px] border border-teal-500 hover:border-2 h-[380px] overflow-hidden rounded-lg transition-all'>
-      {/* Alert message */}
-    {showAlert && (
-      <div className="absolute top-0 left-0 right-0 bg-green-500 text-white text-center p-2 z-50">
-        Item added to cart!
-      </div>
-    )}
       <Link to={''}>
         <img
           src={post.image}
@@ -133,11 +77,9 @@ export default function SeekerCartPost({ ShowAddcart, showApply, showDelete, pos
           <p>{formatDate(post.createdAt)}</p>
         </div>
         {showApply && showDelete && (
-          <div className='flex items-center text-center gap-2 ml-5'>
-            <Button className='px-8 bg-slate-500' onClick={handleCommentModalOpen}>
-              comments
-            </Button>
-            <Button className='px-10' onClick={handleApplyButtonClick}>
+          <div className='flex items-center gap-2 ml-5'>
+            <Button className='px-10 bg-red-900 hover:bg-red-700'>Delete</Button>
+            <Button className='px-10' onClick={handleModalOpen}>
               Apply
             </Button>
           </div>
@@ -148,13 +90,12 @@ export default function SeekerCartPost({ ShowAddcart, showApply, showDelete, pos
           </Button>
         )}
         {showApply && !showDelete && (
-          <Button className='px-20' onClick={handleModalOpen}>
+          <Button className='px-20' onClick={''}>
             Apply
           </Button>
         )}
       </div>
       <CompanyDetailsModal isOpen={isModalOpen} onClose={handleModalClose} showSendCVLink={true} />
-      <DashComments isOpen={isCommentModalOpen} onClose={handleCommentModalClose} postId={post._id}/>
     </div>
   );
 }
