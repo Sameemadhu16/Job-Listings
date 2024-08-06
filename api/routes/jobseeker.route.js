609f5f84b3ad0059fac2082e7ae8736517b3a3fc
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-08-06 14:33:47 +0530

    create seeker profile ui

--- api/routes/jobseeker.route.js
+++ api/routes/jobseeker.route.js
@@ -1,13 +1,7 @@
 import express from 'express';
-import { deleteSeeker, getJobSeekerByID, getJobSeekers, seekerSignout, updateSeeker } from '../controllers/jobseeker.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.get('/get',getJobSeekers);  //retrive jobSeekers
-router.get('/get/:userId',verifyToken,getJobSeekerByID)
-router.put('/update/:userId',verifyToken,updateSeeker);
-router.delete('/delete/:userId',verifyToken,deleteSeeker);
-router.post('/seeker-signout',seekerSignout);
+router.put('/update:',UpdateSeeker);
 
 export default router;
