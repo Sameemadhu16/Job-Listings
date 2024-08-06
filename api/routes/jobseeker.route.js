84785829cb6bfb28d62a8ce90949fa120b764ea0
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-08-06 14:33:47 +0530

    create seeker profile ui

--- api/routes/jobseeker.route.js
+++ api/routes/jobseeker.route.js
@@ -1,9 +1,7 @@
 import express from 'express';
-import { updateSeeker } from '../controllers/jobseeker.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.put('/update/:userId',verifyToken,updateSeeker);
+router.put('/update:',UpdateSeeker);
 
 export default router;
--- client/src/components/Seeker.DashSidebar.jsx
+++ client/src/components/Seeker.DashSidebar.jsx
@@ -5,7 +5,6 @@ import {Link, useLocation} from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux'
 import { FaShoppingCart } from 'react-icons/fa'
-import { FaUser } from 'react-icons/fa'
 
 export default function SeekerDashSidebar() {
     const location = useLocation();
@@ -56,7 +55,7 @@ export default function SeekerDashSidebar() {
           </Link>
           
           <Link to="/seeker-dashboard?tab=profile">
-            <Sidebar.Item active={tab === "profile"} icon={FaUser} as="div">
+            <Sidebar.Item active={tab === "profile"} icon={FaShoppingCart} as="div">
               Profile
             </Sidebar.Item>
           </Link>
