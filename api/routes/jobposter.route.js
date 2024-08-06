d44937c325b94c5d0b9dd0c8ce23fdd1b9328163
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-08-06 17:02:38 +0530

    Add Get job poster using By id function and job poster update function

--- api/routes/jobposter.route.js
+++ api/routes/jobposter.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getJobPosters, updateJobpoter } from '../controllers/jobposter.controller.js';
+import { getJobPosterByID, getJobPosters, updateJobposter} from '../controllers/jobposter.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
 const router = express.Router();
 
 router.get('/get',verifyToken,getJobPosters);  //retrive jobPosters
-router.put('/update/:jobposterId',updateJobpoter,verifyToken)
+router.get('/get/:userId',verifyToken,getJobPosterByID)
+router.put('/update/:userId',verifyToken,updateJobposter)
 
 export default router;
