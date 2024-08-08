ebd9dc1ea87d8ada39870ce25cfe7aa9659399ae
irumi <irumiabeywickrama@gmail.com>
2024-08-08 20:42:15 +0530

    complete comment section backend and frontend

--- api/routes/seeker.route.js
+++ api/routes/seeker.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addappliedjobs, addcart, getapplied, getcart, getJobs } from '../controllers/seeker.controller.js'
+import { addappliedjobs, addcart, deleteCartpost, getapplied, getcart, getJobs } from '../controllers/seeker.controller.js'
 
 const router = express.Router();
 router.get('/getjobs', getJobs);
@@ -8,4 +8,6 @@ router.post('/addcart/:userId', addcart);
 router.post('/addapplied/:userId', addappliedjobs);
 router.get('/getcart/:userId', getcart);
 router.get('/getapplied/:userId', getapplied);
+router.delete('/deletecartpost/:cartPostIdToDelete/:currentUserId',deleteCartpost)
+
 export default router;
