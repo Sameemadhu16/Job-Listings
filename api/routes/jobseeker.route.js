1851fda614b8fb932b0a8ce41b602fd26ba26428
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-08-08 09:12:34 +0530

    add update seeker api route

--- api/routes/jobseeker.route.js
+++ api/routes/jobseeker.route.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { updateSeeker } from '../controllers/jobseeker.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.put('/update:',UpdateSeeker);
+router.put('/update/:userId',verifyToken,updateSeeker);
 
 export default router;
