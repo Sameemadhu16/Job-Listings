812162ba682c90af61f4648c1d125be865a31815
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-08-11 11:23:36 +0530

    update sign up

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -60,7 +60,7 @@ export const signin = async (req, res, next) => {
 
 
 
-        const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin, isModerator: validUser.isModerator }, process.env.JWT_SECRET);
+        const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin, }, process.env.JWT_SECRET);
 
 
 
