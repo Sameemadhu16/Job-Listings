9555af17bb2136f85abe335550db4d09b85dad91
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-08-18 10:54:49 +0530

    add dark mode functionalities

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,18 @@
-import { Avatar, Dropdown, Navbar, TextInput, Button } from "flowbite-react";
+import { Avatar, Dropdown, Navbar, TextInput, Button, theme } from "flowbite-react";
 import logo from '../images/Jobpilot.png';
 import {AiOutlineSearch} from 'react-icons/ai'
 import { IoMdNotifications } from "react-icons/io";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
 
   const {currentUser} = useSelector(state => state.user)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const getProfilePath = (role) => {
     if (role === 'jobPoster') {
@@ -18,10 +23,27 @@ export default function Header() {
     
   };
 
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/jobseeker/seeker-signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <Navbar fluid rounded className="border-b-2">
       <Navbar.Brand href="/">
-        <img src={logo} className="mr-3 h-6 sm:h-14" alt="jobpilot-logo" />
+        <img src={logo} className="mr-3 h-6 sm:h-14 rounded-full" alt="jobpilot-logo" />
         <span className="self-center whitespace-nowrap text-xl font-bold dark:text-white">Jobpilot</span>
       </Navbar.Brand>
       <form>
@@ -37,6 +59,15 @@ export default function Header() {
             <IoMdNotifications/>
         </Button>
       <div className="flex md:order-2">
+        <Button
+          className='w-12 h-10 hidden sm:inline mr-2'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaSun /> : <FaMoon />}
+        </Button>
+
         {currentUser ? (
           <Dropdown
           arrowIcon={false}
@@ -55,7 +86,7 @@ export default function Header() {
             <Dropdown.Divider />
           
           <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
+          <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
         </Dropdown>
           ):(
             <Link to='/sign-in'>
