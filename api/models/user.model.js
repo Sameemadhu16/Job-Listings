0b6ea445e42c9f338d991894572b225b259193ca
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-08-20 13:57:38 +0530

    create update job poster profile functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,27 +1,28 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 
-        fullname: {
-            type: String,
-            required: true,
-        },
+    fullname: {
+        type: String,
+        required: true,
+    },
 
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 
-        password: {
-            type: String,
-            required: true,
-        },
+    password: {
+        type: String,
+        required: true,
+    },
+
 
 
         role: {
