8cc4a5cb87c5a0d6e907e50c0c1f854a442d00ff
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-08-20 13:57:38 +0530

    create update job poster profile functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,116 +1,118 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 
-        fullname: {
-            type: String,
-            required: true,
-        },
+    fullname: {
+        type: String,
+        required: true,
+    },
 
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
 
-        password: {
-            type: String,
-            required: true,
-        },
+    password: {
+        type: String,
+        required: true,
+    },
 
-        role: {
-            type: String,
-            enum: ['jobPoster', 'jobSeeker'],
-            required: true,
-        },
+    role: {
+        type: String,
+        enum: ['jobPoster', 'jobSeeker'],
+        required: true,
+    },
 
-        gender: {
-            type: String,
-        },
-        mobilenumber: {
-            type: String,
-            required: true,
-        },
-        birthday: {
-            trpe: String,
-        },
-        maritalStatus: {
-            type: String,
-            enum: ['married', 'unmarried'],
-        },
-        experience: {
-            type : String,
-        },
-        profilePicture: {
-            type: String,
-            default:
-              'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-          },
-          isAdmin: {
-            type : Boolean,
-            default: false,
-          },
+    gender: {
+        type: String,
+    },
+    mobilenumber: {
+        type: String,
+        required: true,
+    },
+    birthday: {
+        type: String,
+    },
+    maritalStatus: {
+        type: String,
+        enum: ['married', 'unmarried'],
+    },
+    experience: {
+        type: String,
+    },
+    profilePicture: {
+        type: String,
+        default:
+            'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 
-          isModerator: {
-            type : Boolean,
-            default: false,
-          },
-       
+    isModerator: {
+        type: Boolean,
+        default: false,
+    },
 
-        //Feild specific to job posters
-        companyName: {
-            type: String,
-            required : function(){
-                return this.role === 'jobPoster';
-            }
-            
-        },
-        
-        biography: {
-            type: String,
-            required : function(){
-                return this.role === 'jobPoster';
-            }
-        },
-        coverLetter: {
-            type: String,
-            required : function (){
-                return this.role === 'jobPoster';
-            }
-        },
 
-        //Feild specific to job seeker
-        cv: {
-            type: String,
-            
-        },
-            
-        skills: {
-            type: [String],
-            required: function(){
-                return this.role === 'jobSeeker';
-            }
-       
+    //Feild specific to job posters
+    companyName: {
+        type: String,
+        required: function () {
+            return this.role === 'jobPoster';
+        }
 
-        },
+    },
 
-        cart: {
-            type: [String],
-            
-       
-        },
-        appliedjobs: {
-            type: [String],
-            
-
+    biography: {
+        type: String,
+        default: "I've been passionate about graphic design and digital art from an early age with a keen interest in Website and Mobile Application User Interfaces...",
+        required: function () {
+            return this.role === 'jobPoster';
         }
     },
-    {timestamps: true}
+    coverLetter: {
+        type: String,
+        default: "Dear ABC?",
+        required: function () {
+            return this.role === 'jobPoster';
+        }
+    },
+
+    //Feild specific to job seeker
+    cv: {
+        type: String,
+
+    },
+
+    skills: {
+        type: [String],
+        required: function () {
+            return this.role === 'jobSeeker';
+        }
+
+
+    },
+
+    cart: {
+        type: [String],
+
+
+    },
+    appliedjobs: {
+        type: [String],
+
+
+    }
+},
+    { timestamps: true }
 );
 
 const User = mongoose.model('User', userSchema);
