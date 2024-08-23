5ee1b0dde565be457e1437595742697fc42aaf7e
Shashika Ekanayaka <2022cs050@stu.ucsc.cmb.ac.lk>
2024-08-23 23:02:28 +0530

    create add contact api in contact.controller file

--- api/controllers/contact.controller.js
+++ api/controllers/contact.controller.js
@@ -1,4 +1,18 @@
 export const addContact = async (req, res, next) => {
-    
+   try {
+    const {name , email, message}= req.body;
+
+    const newContact = new Contact({
+        name,
+        email,
+        message
+    });
+    await newContact.save();
+    res.status(200).json(newContact);
+
+
+   } catch (error) {
+    next(error);
+   }
 }
 
