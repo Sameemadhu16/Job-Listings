4555669f72820df757c1207896790584bd53ad11
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-08-30 16:52:13 +0530

    add saved candidate route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,6 +28,7 @@ import SeekerProfile from './components/Seeker.Profile';
 
 import PosterDashMyJobs from './components/Poster.DashMyJobs';
 import PosterDashEmployeeProfile from './components/Poster.DashEmployeeProfile';
+import PosterDashSavedCandidate from './components/Poster.DashSavedCandidate';
 
 
 
@@ -55,6 +56,7 @@ export default function App() {
             <Route path="social-media" element={<PosterSocialMediaInfo />} />
             <Route path="account-setting" element={<PosterCompanyInfo />} />
           </Route>
+          <Route path="saved-candidate" element={<PosterDashSavedCandidate />}></Route>
           <Route path="settings" element={<PosterSetting />}>
             <Route index element={<PosterCompanyInfo />} />
           </Route>
