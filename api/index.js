780f8f0848447593d9c7927228a8c02d0a45d074
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-09-16 20:35:40 +0530

    coneect socket server

--- api/index.js
+++ api/index.js
@@ -6,12 +6,13 @@ import seekerRoutes from './routes/seeker.route.js'
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/post.route.js'
 import contactRoutes from './routes/contact.route.js'
-
 import commentRoutes from './routes/comment.route.js'
 import jobseekerRoutes from './routes/jobseeker.route.js'
 import jobposterRoutes from './routes/jobposter.route.js'
 import responseRoutes from './routes/savecandidate.js'
 import cors from 'cors'
+import {createServer} from 'http';
+import {Server} from 'socket.io';
 
 dotenv.config();
 
@@ -28,30 +29,42 @@ mongoose.connect("mongodb+srv://ilakshitha7921:ilakshitha7921@cluster0.gfhczos.m
 
 
 const app = express();
+const httpServer = createServer(app);
 
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods : ['GET' , 'POST'],
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected', socket.id);
+
+  // Handle socket events here
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+  socket.on('chatMessage', (msg) => {
+    console.log('Message received: ', msg);
+    io.emit('chatMessage', msg); // Broadcast message to all clients
+  });
+});
+
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  
-}));
-app.listen(4500, () => {
-  console.log('Server is running port 4500');
-});
+app.use(cors({}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/jobposter', jobposterRoutes);
 app.use('/api/seeker', seekerRoutes);
 app.use('/api/response',responseRoutes);
-
-
-
-
 app.use('/api/post',postRoutes);
 app.use('/api/jobseeker',jobseekerRoutes);
 app.use('/api/comment', commentRoutes)
 app.use('/api/post', postRoutes);
-
-
 app.use('/api/contact', contactRoutes);
 
 
@@ -66,6 +79,11 @@ app.use((err, req, res, next) => {
     message,
   });
 
+});
+
+// Start server with socket.io
+httpServer.listen(4500, () => {
+  console.log('Server is running on port 4500');
 });
 
 
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from "react-redux";
-import { Avatar , TextInput } from 'flowbite-react';
+import { Avatar , TextInput , Label } from 'flowbite-react';
 import { FiSend, FiHash } from 'react-icons/fi'; // React icons for hash and send button
 
 export default function Chat() {
@@ -9,38 +9,48 @@ export default function Chat() {
 console.log(currentUser)
     return (
         <div className='min-h-screen'>
-            <div className='p-10 bg-blue-50'>
+            <div className='p-10 bg-blue-50 '>
                 <h1 className='text-3xl font-semibold text-black'>Chat</h1>
-                <p>Chat with users</p>
-                <div className='bg-white rounded-lg'>
-                    <div className='p-5 border-b-2 border-b-slate-400'>
-                        <div className='flex items-center gap-2 '>
-                            <Avatar alt="User settings" img={currentUser.currentUser.profilePicture} rounded />
-                            <p>{currentUser.currentUser.username}</p>
+                <p className='text-slate-600 mt-2'>Connect and chat with users in real-time, where every conversation brings us closer together!</p>
+                <div className='flex justify-center items-center'>
+                    <div className='w-full md:w-3/4 flex flex-col mt-10'>
+                        <div className='bg-white rounded-lg'>
+                            <div className='p-5 border-b-2 border-b-slate-400'>
+                                <div className='flex items-center gap-2'>
+                                    <Avatar alt="User settings" img={currentUser.currentUser.profilePicture} rounded />
+                                    <p>{currentUser.currentUser.username}</p>
+                                    {
+                                        currentUser.role === 'jobSeeker' ?
+                                        <Label className="text-sm font-bold text-blue-600 border-2 border-blue-400 dark:text-blue-200 dark:border-blue-200 px-2 py-1">Finder</Label> :
+                                        <Label className="text-sm font-bold text-blue-600 border-2 border-blue-400 dark:text-blue-200 dark:border-blue-200 px-2 py-1">Poster</Label>
+                                    }
+                                </div>
+                            </div>
+                            <div className='min-h-screen bg-gray-100'>
+                                {/* chat body */}
+                            </div>
+                            <div>
+                                <form className="flex items-center w-full p-2 bg-white border-t border-gray-200">
+                                    {/* Icon on the left */}
+                                    <FiHash className="text-gray-400 text-xl mx-2" />
+
+                                    {/* Input field */}
+                                    <input
+                                        type="text"
+                                        placeholder="Start typing..."
+                                        className="flex-grow bg-transparent border-none focus:ring-0 focus:outline-none text-sm text-gray-600 placeholder-gray-400 px-2"
+                                    />
+
+                                    {/* Send button */}
+                                    <button type="submit" className="text-blue-500 hover:bg-blue-100 p-2 rounded-full">
+                                        <FiSend className="text-xl" />
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                    <div className='min-h-screen bg-gray-100 rounded-lg'>
-                        {/* chat body */}
-                    </div>
-                    <div>
-                    <form className="flex items-center w-full p-2 bg-white border-t border-gray-200">
-                        {/* Icon on the left */}
-                        <FiHash className="text-gray-400 text-xl mx-2" />
-
-                        {/* Input field */}
-                        <input
-                            type="text"
-                            placeholder="Start typing..."
-                            className="flex-grow bg-transparent border-none focus:ring-0 focus:outline-none text-sm text-gray-600 placeholder-gray-400 px-2"
-                        />
-
-                        {/* Send button */}
-                        <button type="submit" className="text-blue-500 hover:bg-blue-100 p-2 rounded-full">
-                            <FiSend className="text-xl" />
-                        </button>
-                    </form>
-                    </div>
                 </div>
+
             </div>
         </div>
     )
