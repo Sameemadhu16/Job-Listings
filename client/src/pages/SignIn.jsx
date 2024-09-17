ad55a72b92d720d4e242dc4f1b8e3087249e229c
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-09-17 20:03:54 +0530

    make changes

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -69,7 +69,7 @@ const SignIn = () => {
                         <div className='w-1/2 dark:bg-slate-600 dark:text-black bg-white p-8 z-10 rounded-lg rounded-r-none'>
                             <div className="max-w-md dark:text-white">
                                 <div className="">
-                                    <img src={logo} alt="Logo" className="h-28 " />
+                                    <img src={logo} alt="Logo" className="h-28 rounded-full" />
                                 </div>
                                 <div className='text-start'>
                                     <h2 className="text-3xl font-bold font-sans">Welcome Back</h2>
